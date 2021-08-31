9015a3bc654ba0850916c49cd3d3701a180dcf41
msakuta <masahiro.sakuta@gmail.com>
2021-08-31 23:05:25 +0900

    Implement std::error::Error for geos::Error

--- src/error.rs
+++ src/error.rs
@@ -11,6 +11,8 @@ pub enum Error {
     GenericError(String),
 }
 
+impl std::error::Error for Error {}
+
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
